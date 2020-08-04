184739a356b5db452d6e39833dde9eea7a5d1945
Andi Corradi <Andi@corradi.ch>
2020-08-04 10:19:19 +0200

    Interation 1 Fertiggestellt

--- Rechner/Program.cs
+++ Rechner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,25 @@ namespace Rechner
     {
         static void Main(string[] args)
         {
+            // Hole Summanden
+            Console.Write("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // convertieren in Gleitkommazhlen
+
+            double ersterSummandAlsZahl = Convert.ToSingle(ersterSummand);
+            double zweiterSummandAlsZahl = Convert.ToSingle(zweiterSummand);
+
+            // Berechnung ausführen
+
+            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe
+            Console.WriteLine("Die Summe ist: {0}", summe);
+            Console.WriteLine("Drücke den AnyKey um das Programm zu schliessen");
+            Console.ReadLine();
         }
     }
 }
